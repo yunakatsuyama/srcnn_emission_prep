0375866a88587d114752bf9556cac32b224488c4
Yuna Katsuyama <02feuille@gmail.com>
2026-01-30 19:25:13 +0100

    init

---
+++ emi_srcnn.py
@@ -0,0 +1,15 @@
+
+import numpy as np 
+import pandas as pd 
+import xarray as xr 
+#import math
+import matplotlib.pyplot as plt
+from cmcrameri import cm
+#import cartopy.crs as ccrs
+#import cartopy.feature as cfeature
+import matplotlib.colors as mcolors
+#import matplotlib.image as mpimg
+#from scipy.interpolate import RegularGridInterpolator
+
+import xesmf as xe
+
